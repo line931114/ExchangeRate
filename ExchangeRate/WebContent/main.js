ec9c38f3dc18f2f7ee1e3bbb17638553df2d406c
이광호 <dlrhkdgh3333@naver.com>
2021-12-27 20:55:12 +0900

    12_27 Exchange Rate 2

--- ExchangeRate/WebContent/main.js
+++ ExchangeRate/WebContent/main.js
@@ -18,14 +18,21 @@ let currencyRatio = {
 		unit:"동"
 	}
 };
+/*let json = JSON.stringify(currencyRatio,['USD']);
+console.log(json);
+
+json = JSON.stringify(currencyRatio, (key, value) => {
+	console.log(`key:${key}, value: ${value}`);
+	return value;
+});*/
 
 let fromCurrency = 'USD';
-let toCurrency = 'USD';
+let toCurrency = 'KRW';
 let fromMoney = "1";
 
 console.log(currencyRatio.USD.KRW);
 console.log(currencyRatio)
-//console.log(currencyRatio['fromCurrency']['toCurrency']);
+console.log(currencyRatio[fromCurrency][toCurrency]);
 
 //  1. console.log(currencyRatio.USD.unit);
 // currencyRatio 안에 USD 안에 unit 의 값
@@ -39,49 +46,34 @@ document.querySelectorAll("#from-currency-list a").forEach(menu=>menu.addEventLi
 	//3. 선택된 currency값을 변수에 저장해준다.
 	fromCurrency = this.textContent;
 	console.log(fromCurrency);
-	document.getElementById("money").innerText = fromCurrency;
-	
+	//document.getElementById("money").innerText = fromCurrency;	
+	convert("from");
 
 }));
-var m = 1;
-var u = "달러";
 document.querySelectorAll("#to-currency-list a").forEach(menu=>menu.addEventListener("click",function(){
 	
 	document.getElementById("to-button").textContent=this.textContent;
 	toCurrency= this.textContent;
 	console.log(toCurrency);
-	document.getElementById("money1").innerText = toCurrency;
-	
-	//console.log(currencyRatio.fromCurrency.toCurrency);
-	console.log(currencyRatio.USD.KRW)
-	//만약 fromCurrency = usd고, toCurrency = krw일경우
-	
-	
-	//console.log(currencyRatio.fromCurrency.toCurrency);
-	//이런 식으로 값을 이용해서 꺼내고 싶은데 fromCurrency,toCurrency값이 undefined라고 나와요..
-	
-	//console.log(currencyRatio.toCurrency);
-	
-		if(fromCurrency=="USD" && toCurrency=="KRW"){
-		 m = 1190.77;
-		 u = "원(환율금액)";
-		
-	}else if(fromCurrency=="USD" && toCurrency=="VDN"){
-		m=22970.00;
-		u = "동(환율금액)";
-	}
-	
+	//document.getElementById("money1").innerText = toCurrency;
+	convert("to");
+	console.log(currencyRatio[toCurrency].unit);
+
 }));
 
-
-
-
-function fromMoeney() {
-	var money = document.getElementById("fromMoney").value;
-	document.getElementById("result").innerText = (m*money)+u;
+function convert(type) {
+	let money= 0	;
+	if( type=="from"){
+		money = document.getElementById("from-Money").value;
+		let exMoney = money * currencyRatio[fromCurrency][toCurrency]
+		document.getElementById("to-Money").value = exMoney;
+		document.getElementById("money").innerText = currencyRatio[fromCurrency].unit;	
 	
+	}else{
+		money = document.getElementById("to-Money").value;
+		let exreMoney= money * currencyRatio[toCurrency][fromCurrency]
+		document.getElementById("from-Money").value = exreMoney;	
+		document.getElementById("money1").innerText = currencyRatio[toCurrency].unit;
+	}
 };
 
-
-//from-currency-list 안에 있는 a태그에 이벤트를 줄거다.
-//
